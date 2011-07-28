73f5a11da7b787d1a9caf3da9fee68bda6b7f828
Jake Turner <jake@crytek.com>
2011-07-28 15:52:47 +0100

    highlight in blue connections which are active highlight in red the current connection

--- hmgui.py
+++ hmgui.py
@@ -6,6 +6,10 @@ import hmclient
 class HMGUI(Tkinter.Frame):
 	def __init__(self, master = None):
 		Tkinter.Frame.__init__(self, master)
+		self.TEXT_clientNames = []
+		self.TEXT_clientIPs = []
+		self.TEXT_clientNetIDs = []
+		self.TEXT_clientNetPorts = []
 		self.grid()
 		self.createWidgets()
 		self.appTitle = "HM Tool"
@@ -49,47 +53,70 @@ class HMGUI(Tkinter.Frame):
 		if selected:
 			fgColour = "red"
 		else:
-			fgColour = "black"
+			if self.serverConnection == None:
+				fgColour = "black"
+			else:
+				fgColour = "blue"
+
 		self.TEXT_serverName.config(fg = fgColour)
 		self.TEXT_serverIP.config(fg = fgColour)
-		self.setClientSelected(None)
+		if selected == True:
+			self.setClientSelected(None)
+			self.updateClientsDisplay()
 
 	def clearClients(self):
-		self.TEXT_clientNames.config(state = Tkinter.NORMAL)
-		self.TEXT_clientNames.delete(1.0, Tkinter.END)
-		self.TEXT_clientNames.config(state = Tkinter.DISABLED)
-		self.TEXT_clientIPs.config(state = Tkinter.NORMAL)
-		self.TEXT_clientIPs.delete(1.0, Tkinter.END)
-		self.TEXT_clientIPs.config(state = Tkinter.DISABLED)
-		self.TEXT_clientNetIDs.config(state = Tkinter.NORMAL)
-		self.TEXT_clientNetIDs.delete(1.0, Tkinter.END)
-		self.TEXT_clientNetIDs.config(state = Tkinter.DISABLED)
-		self.TEXT_clientPorts.config(state = Tkinter.NORMAL)
-		self.TEXT_clientPorts.delete(1.0, Tkinter.END)
-		self.TEXT_clientPorts.config(state = Tkinter.DISABLED)
+		for w in self.TEXT_clientNames:
+			w.config(state = Tkinter.NORMAL)
+			w.delete(1.0, Tkinter.END)
+			w.config(state = Tkinter.DISABLED)
+		for w in self.TEXT_clientIPs:
+			w.config(state = Tkinter.NORMAL)
+			w.delete(1.0, Tkinter.END)
+			w.config(state = Tkinter.DISABLED)
+		for w in self.TEXT_clientNetIDs:
+			w.config(state = Tkinter.NORMAL)
+			w.delete(1.0, Tkinter.END)
+			w.config(state = Tkinter.DISABLED)
+		for w in self.TEXT_clientPorts:
+			w.config(state = Tkinter.NORMAL)
+			w.delete(1.0, Tkinter.END)
+			w.config(state = Tkinter.DISABLED)
 
-	def addClientEntry(self, clientDetail, selected):
+	def addClientEntry(self, clientDetail, selected, connected):
 		if selected:
 			fgColour = "red"
 		else:
-			fgColour = "black"
+			if connected:
+				fgColour = "blue"
+			else:
+				fgColour = "black"
 
-		self.TEXT_clientNames.config(state = Tkinter.NORMAL, fg = fgColour)
-		self.TEXT_clientNames.insert(Tkinter.END, clientDetail[0])
-		self.TEXT_clientNames.insert(Tkinter.END, "\n")
-		self.TEXT_clientNames.config(state = Tkinter.DISABLED)
-		self.TEXT_clientIPs.config(state = Tkinter.NORMAL, fg = fgColour)
-		self.TEXT_clientIPs.insert(Tkinter.END, clientDetail[1])
-		self.TEXT_clientIPs.insert(Tkinter.END, "\n")
-		self.TEXT_clientIPs.config(state = Tkinter.DISABLED)
-		self.TEXT_clientNetIDs.config(state = Tkinter.NORMAL, fg = fgColour)
-		self.TEXT_clientNetIDs.insert(Tkinter.END, clientDetail[2])
-		self.TEXT_clientNetIDs.insert(Tkinter.END, "\n")
-		self.TEXT_clientNetIDs.config(state = Tkinter.DISABLED)
-		self.TEXT_clientPorts.config(state = Tkinter.NORMAL, fg = fgColour)
-		self.TEXT_clientPorts.insert(Tkinter.END, clientDetail[3])
-		self.TEXT_clientPorts.insert(Tkinter.END, "\n")
-		self.TEXT_clientPorts.config(state = Tkinter.DISABLED)
+		index = 0
+		for w in self.TEXT_clientNames:
+			clientName = w.get(1.0, Tkinter.END)
+			clientName = clientName.strip()
+			print "clientName:"+clientName
+			if len(clientName) == 0:
+				print "Empty client name"
+				break
+			index += 1
+
+		if index >= len(self.TEXT_clientNames):
+			print "No space in client names"
+			return
+
+		self.TEXT_clientNames[index].config(state = Tkinter.NORMAL, fg = fgColour)
+		self.TEXT_clientNames[index].insert(Tkinter.END, clientDetail[0])
+		self.TEXT_clientNames[index].config(state = Tkinter.DISABLED)
+		self.TEXT_clientIPs[index].config(state = Tkinter.NORMAL, fg = fgColour)
+		self.TEXT_clientIPs[index].insert(Tkinter.END, clientDetail[1])
+		self.TEXT_clientIPs[index].config(state = Tkinter.DISABLED)
+		self.TEXT_clientNetIDs[index].config(state = Tkinter.NORMAL, fg = fgColour)
+		self.TEXT_clientNetIDs[index].insert(Tkinter.END, clientDetail[2])
+		self.TEXT_clientNetIDs[index].config(state = Tkinter.DISABLED)
+		self.TEXT_clientPorts[index].config(state = Tkinter.NORMAL, fg = fgColour)
+		self.TEXT_clientPorts[index].insert(Tkinter.END, clientDetail[3])
+		self.TEXT_clientPorts[index].config(state = Tkinter.DISABLED)
 
 	def setClientSelected(self, clientDetail):
 		self.selectedClient = clientDetail
@@ -126,19 +153,26 @@ class HMGUI(Tkinter.Frame):
 				self.setServerSelected(True)
 				self.consolePrint("is a server")
 
-			if self.currentConnection.isClient():
-				print "A client " + HTTPServerName + ":" + HTTPServerPort
+			result = self.currentConnection.isClient()
+			if result[0]:
+				clientName = result[1]
 				clientIP = HTTPServerName
 				serverBasePort = int(self.serverInfo[3])
 				clientNetID = str(int(HTTPServerPort) - serverBasePort)
-				print "clientNetID="+clientNetID
+				clientPort = HTTPServerPort
+				self.consolePrint("A client: Name:" + clientName +" IP:" + clientIP + " NetID:" + clientNetID + " Port:" + clientPort)
+				self.currentConnection.setDetail(clientName, clientIP, clientNetID, clientPort)
+
+				selectedClient = None
 				for cd in self.clientDetails:
+					# Match on NetID only!
 					if cd[2] == clientNetID:
 						cd[3] = HTTPServerPort
+						selectedClient = cd
 						break;
+				self.consolePrint(str(selectedClient))
+				self.setClientSelected(selectedClient)
 				self.updateClientsDisplay()
-				self.setClientSelected(cd)
-				self.consolePrint("is a client")
 
 			self.consolePrint("END #####################################################", noprefix = True)
 
@@ -193,11 +227,31 @@ class HMGUI(Tkinter.Frame):
 		self.clearClients()
 		for cd in self.clientDetails:
 			selected = False
+			connected = False
 			if self.selectedClient != None:
-				# Match on port number only at the moment but should be IP + port number
-				if cd[3] == self.selectedClient[3]:
+				print "Selected or not"
+				print "cd[0]:", cd[0], " self.selectedClient[0]:", self.selectedClient[0]
+				print "cd[1]:", cd[1], " self.selectedClient[1]:", self.selectedClient[1]
+				print "cd[2]:", cd[2], " self.selectedClient[2]:", self.selectedClient[2]
+				print "cd[3]:", cd[3], " self.selectedClient[3]:", self.selectedClient[3]
+				# Match on name & IP & net ID & port number
+				if cd[0] == self.selectedClient[0] and cd[1] == self.selectedClient[1] and cd[2] == self.selectedClient[2] and cd[3] == self.selectedClient[3]:
 					selected = True
-			self.addClientEntry(cd, selected)
+
+			for con in self.connections:
+				# Match on name & IP & net ID & port number 
+				if con != None:
+					conDetail = con.getDetail()
+					print "Connected or not"
+					print "cd[0]:", cd[0], " conDetail[0]:", conDetail[0]
+					print "cd[1]:", cd[1], " conDetail[1]:", conDetail[1]
+					print "cd[2]:", cd[2], " conDetail[2]:", conDetail[2]
+					print "cd[3]:", cd[3], " conDetail[3]:", conDetail[3]
+					if conDetail[0] == cd[0] and conDetail[1] == cd[1] and conDetail[2] == cd[2] and conDetail[3] == cd[3]:
+							connected = True
+							break
+
+			self.addClientEntry(cd, selected, connected)
 
 	def getClientIPs(self):
 		if self.testServerConnection("GetClientIPs") == False:
@@ -298,7 +352,7 @@ class HMGUI(Tkinter.Frame):
 	def jakeTest(self):
 		oldConnection = self.currentConnection
 		self.currentConnection = self.serverConnection
-		self.sendCommandInternal("jake", "129 140 141")
+		self.sendCommandInternal("jake", "129 140 150")
 		self.currentConnection = oldConnection
 
 	def returnKey(self, event):
@@ -383,20 +437,30 @@ class HMGUI(Tkinter.Frame):
 		self.LABEL_clientPort.grid(row = curRow, column = curCol, columnspan = 1)
 		curCol += 1
 
-		curRow += 1
-		curCol = 0
-		self.TEXT_clientNames = Tkinter.Text(self, state = Tkinter.DISABLED, width = serverClientBoxWidth, height = 16, bg = disableBGcolour)
-		self.TEXT_clientNames.grid(row = curRow, column = curCol, rowspan=16)
-		curCol += 1
-		self.TEXT_clientIPs = Tkinter.Text(self, state = Tkinter.DISABLED, width = serverClientIPBoxWidth, height = 16, bg = disableBGcolour)
-		self.TEXT_clientIPs.grid(row = curRow, column = curCol, rowspan=16)
-		curCol += 1
-		self.TEXT_clientNetIDs = Tkinter.Text(self, state = Tkinter.DISABLED, width = 6, height = 16, bg = disableBGcolour)
-		self.TEXT_clientNetIDs.grid(row = curRow, column = curCol, rowspan=16)
-		curCol += 1
-		self.TEXT_clientPorts = Tkinter.Text(self, state = Tkinter.DISABLED, width = 6, height = 16, bg = disableBGcolour)
-		self.TEXT_clientPorts.grid(row = curRow, column = curCol, rowspan=16)
-		curCol += 1
+		numClients = 16
+		self.TEXT_clientNames = []
+		self.TEXT_clientIPs = []
+		self.TEXT_clientNetIDs = []
+		self.TEXT_clientPorts = []
+		for i in range(numClients):
+			curRow += 1
+			curCol = 0
+			TEXT_clientName = Tkinter.Text(self, state = Tkinter.DISABLED, width = serverClientBoxWidth, height = 1, bg = disableBGcolour)
+			TEXT_clientName.grid(row = curRow, column = curCol)
+			self.TEXT_clientNames.append(TEXT_clientName)
+			curCol += 1
+			TEXT_clientIP = Tkinter.Text(self, state = Tkinter.DISABLED, width = serverClientIPBoxWidth, height = 1, bg = disableBGcolour)
+			TEXT_clientIP.grid(row = curRow, column = curCol)
+			self.TEXT_clientIPs.append(TEXT_clientIP)
+			curCol += 1
+			TEXT_clientNetID = Tkinter.Text(self, state = Tkinter.DISABLED, width = 6, height = 1, bg = disableBGcolour)
+			TEXT_clientNetID.grid(row = curRow, column = curCol)
+			self.TEXT_clientNetIDs.append(TEXT_clientNetID)
+			curCol += 1
+			TEXT_clientPort = Tkinter.Text(self, state = Tkinter.DISABLED, width = 6, height = 1, bg = disableBGcolour)
+			TEXT_clientPort.grid(row = curRow, column = curCol)
+			self.TEXT_clientPorts.append(TEXT_clientPort)
+			curCol += 1
 
 		curRow = textSavedRow
 		curCol = 4
